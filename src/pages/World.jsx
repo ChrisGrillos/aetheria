82283f453facaec0ac0c961c4d698b2b09894165
Chris Grillos <cmgrillos529@gmail.com>
2026-03-04 05:28:34 +0000

    File changes

--- src/pages/World.jsx
+++ src/pages/World.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import { useState, useEffect, useRef, useCallback } from "react";
 import { base44 } from "@/api/base44Client";
+
 import { createPageUrl } from "@/utils";
 import { Link } from "react-router-dom";
 import WorldMap from "@/components/world/WorldMap.jsx";
