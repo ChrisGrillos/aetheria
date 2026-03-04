6711932c689e98d8fa72b234fb7b571b34ed2811
Chris Grillos <cmgrillos529@gmail.com>
2026-03-04 06:15:09 +0000

    File changes

--- src/components/world/CharacterHUD.jsx
+++ src/components/world/CharacterHUD.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Heart, Coins, Star, Package, Zap, Menu, X } from "lucide-react";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { calculateDerivedStats } from "@/components/shared/charUtils";
